1a72689803ed6f9178c543db11a19db2bff4f6b2
YOOJS1205 <junsang8921@gmail.com>
2022-10-03 23:15:51 +0900

    Feat: 투두리스트 UI 구현

---
+++ todo/index.js
@@ -0,0 +1,5 @@
+// dom
+const $todayInput = document.querySelector(".today-input");
+const $tomorrowInput = document.querySelector(".tomorrow-input");
+const $todayButton = document.querySelector(".today-button");
+const $tomorrowButton = document.querySelector(".tomorrow-button");
